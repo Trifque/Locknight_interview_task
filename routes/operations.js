c8a3776a409aa3f6f4751c78e1399b3f10d6d639
Aleksandar Trifunović <alekstrif@yahoo.com>
2026-04-06 17:19:04 +0200

    feat: added POST route

--- routes/operations.js
+++ routes/operations.js
@@ -5,4 +5,34 @@ const router = express.Router();
 //Loading task data and a helper function / Ucitavam podatke za task-ove i pomocnu funkciju
 const { tasks, getNextID} = require("../data/tasks");
 
+
+
+//POST - Create a new task / Kreiraj novi task
+
+router.post("/", (request, result) => {
+    // Taking the title from the body / Vadimo naslov iz tela
+    const title = request.body.title;
+
+    //Checking if there is a title / Provera da li ima naslova
+    if(typeof title !== "string" || title.trim() === ""){
+        return result.status(400).send("Missing title/ Fali naslov");
+    }
+
+    //Create the new task with the given title/ Kreiramo novi task sa datim naslovom
+    const newTask = {
+        id: getNextID(),
+        title: title.trim(),
+        completed: false
+    };
+    console.log(newTask);
+
+    //Insert the new task into the array / Ubacujemo novi task u niz
+    tasks.push(newTask);
+
+    //Return positive status 201 / Vrati pozitivan status 201
+    return result.status(201).send("Task successfully created / Task uspesno napravljen");
+});
+
+
+
 module.exports = router;
